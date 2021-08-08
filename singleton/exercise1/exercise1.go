6f208b77ffc76315322fa839f5061e34b1dd474c
clippefuentes <clippefuentes@gmail.com>
2021-08-07 22:26:22 -0600

    remove all practice code

--- singleton/exercise1/exercise1.go
+++ singleton/exercise1/exercise1.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"bufio"
+	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strconv"
 	"sync"
 )
 
-var instance Database
 
 type Database interface {
 	getLevel(name string) int
@@ -25,15 +26,26 @@ func (s *singletonDatabase) getLevel(name string) int {
 }
 
 func readData(path string) (map[string]int, error) {
+	// dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+    // if err != nil {
+    //         log.Fatal(err)
+    // }
+    // fmt.Println(dir)
+	filePath, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(filePath)  // for example /home/user
+
 	ex, err := os.Executable()
+	fmt.Println(ex)
 	if err != nil {
 		panic(err)
 	}
-
-	exPath := filepath.Dir(ex)
-
+	exPath := filepath.Dir(filePath)
+	fmt.Println(exPath)
+	fmt.Println(filePath + path)
 	file, err := os.Open(exPath + path)
-
 	if err != nil {
 		return nil, err
 	}
@@ -43,25 +55,34 @@ func readData(path string) (map[string]int, error) {
 	scanner.Split(bufio.ScanLines)
 
 	result := map[string]int{}
-
 	for scanner.Scan() {
 		k := scanner.Text()
+		fmt.Println(k)
 		scanner.Scan()
 		v, _ := strconv.Atoi(scanner.Text())
+		fmt.Println(v)
 		result[k] = v
 	}
-
 	return result, nil
 }
 
-func GetSingletonDatabase() Database {
+// ever singleton will use sync Once
+var instance Database
+
+func getSingletonDatabase() Database {
 	once.Do(func() {
 		db := singletonDatabase{}
-		heros, err := readData(".\\heros.txt")
+		heros, err := readData("\\heros.txt")
 		if err == nil {
 			db.heros = heros
 		}
 		instance = &db
 	})
 	return instance
+}
+
+func main() {
+	db := getSingletonDatabase()
+	hero1Level := db.getLevel("Sheena")
+	fmt.Println("Sheena level is", hero1Level)
 }
