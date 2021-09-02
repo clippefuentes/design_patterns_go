a8dc3ac5d6ab88b2c62dbdea98b279b282f4166b
clippefuentes <clippefuentes@gmail.com>
2021-09-01 19:34:27 -0600

    finish chain or responsibilities exercise

--- chain-of-responsibilities/exercise1/heroDispatcher.go
+++ chain-of-responsibilities/exercise1/heroDispatcher.go
@@ -46,11 +46,89 @@ func (m *manaShop) execute(h *hero) {
 	}
 	fmt.Println("Refilling mana")
 	h.refillMana = true
-	m.next.execute(h)
+	// m.next.execute(h)s
 }
 
 func (m *manaShop) setNext(next guild) {
 	m.next = next
 }
 
-type 
+type weaponShop struct {
+	next guild
+}
+
+func (w *weaponShop) execute(h *hero) {
+	if h.refillWeapon {
+		fmt.Println("Weapons are already ready")
+		w.next.execute(h)
+		return
+	}
+	fmt.Println("Arrow is refill, Sword is sharped and shield is polished")
+	h.refillWeapon = true
+	w.next.execute(h)
+}
+
+func (w *weaponShop) setNext(next guild) {
+	w.next = next
+}
+
+type foodShop struct {
+	next guild
+}
+
+func (f *foodShop) execute(h *hero) {
+	if h.refillFood {
+		fmt.Println("Food is refilled")
+		f.next.execute(h)
+		return
+	}
+	fmt.Println("refilling food bag")
+	h.refillFood = true
+	f.next.execute(h)
+}
+
+
+func (f *foodShop) setNext(next guild) {
+	f.next = next
+}
+
+func main () {
+	// refillPotion bool
+	// refillMana   bool
+	// refillWeapon bool
+	// refillFood   bool
+	manaShop := &manaShop{}
+	potionShop := &potionShop{}
+
+	potionShop.setNext(manaShop)
+
+	weaponShop := &weaponShop{}
+	weaponShop.setNext(potionShop)
+
+	foodShop := &foodShop{}
+
+	foodShop.setNext(weaponShop)
+	hero := &hero{name: "Clyne"}
+	foodShop.execute(hero)
+}
+
+// func main() {
+
+//     cashier := &cashier{}
+
+//     //Set next for medical department
+//     medical := &medical{}
+//     medical.setNext(cashier)
+
+//     //Set next for doctor department
+//     doctor := &doctor{}
+//     doctor.setNext(medical)
+
+//     //Set next for reception department
+//     reception := &reception{}
+//     reception.setNext(doctor)
+
+//     patient := &patient{name: "abc"}
+//     //Patient visiting
+//     reception.execute(patient)
+// }
